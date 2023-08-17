06c23e840d8053fc42dbf1ce4af42544cc45127c
OPOII <bryancamili198@hotmail.com>
2023-08-16 23:56:31 -0500

    ng on destroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -8,11 +9,16 @@ import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@
 export class PriceComponent implements OnInit,OnDestroy,OnChanges {
   @Input()
   public price:number=0;
+
+  public interval$?: Subscription;
   ngOnInit(): void {
+    // Aquí se crea eventos donde se requieren intervalos para hacer validaciones o metodos infinitos
+    //Es un observable que empieza a emitir valores de manera secuencial basado en un periodo de tiempo
+    this.interval$=interval(1000).subscribe(value=>console.log(`Tick: ${value}`));
     console.log('Componente Hijo: ngOnInit');
   }
   ngOnDestroy(): void {
-
+    this.interval$?.unsubscribe();
     console.log('Componente Hijo: ngOnDestroy');
   }
   ngOnChanges(changes: SimpleChanges): void {
